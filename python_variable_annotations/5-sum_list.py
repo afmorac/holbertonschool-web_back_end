89578b288208ec45c98ece023b1fbb0bc38b2c33
afmorac <andresfmora.catro@gmail.com>
2025-07-30 23:11:05 -0400

    5sums

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Este módulo define una función que suma una lista de floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Devuelve la suma de una lista de números float."""
+    return sum(input_list)
