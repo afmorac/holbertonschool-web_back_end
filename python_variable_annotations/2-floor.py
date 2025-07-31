3bedc1f2940951ffd4fa11df41e7d6cbe7c5a94c
afmorac <andresfmora.catro@gmail.com>
2025-07-30 22:58:34 -0400

    2-floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Este módulo define una función que devuelve el piso de un número float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Devuelve el valor entero menor o igual que n."""
+    return math.floor(n)
