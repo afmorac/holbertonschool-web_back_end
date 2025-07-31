5b37737c7e41f6cbc3f8019c10a662e4b42d7662
afmorac <andresfmora.catro@gmail.com>
2025-07-30 22:54:37 -0400

    1-concat str

---
+++ 1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Este módulo define una función que concatena dos strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Devuelve la concatenación de dos strings."""
+    return str1 + str2
