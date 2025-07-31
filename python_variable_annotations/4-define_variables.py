0a4fd52553a88633ef7cdc2a0059e9d6a709617d
afmorac <andresfmora.catro@gmail.com>
2025-07-30 23:08:45 -0400

    4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Este módulo define variables anotadas con sus tipos."""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
