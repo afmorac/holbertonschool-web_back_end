4aba73e5620f56bda2ecb6aab97f94aadff149fd
afmorac <andresfmora.catro@gmail.com>
2025-07-30 23:06:56 -0400

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Este módulo define una función que convierte un float a string."""
+
+
+def to_str(n: float) -> str:
+    """Devuelve la representación en string de un número float."""
+    return str(n)
