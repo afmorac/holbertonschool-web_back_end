7d4d032bb08c66237bde1529aab9adef15a53bde
Andrés M <andresfmora.castro@gmail.com>
2025-08-02 12:59:08 -0400

    tsk2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Mide el tiempo que tardan 4 async_comprehensions corriendo en paralelo"""
+
+import asyncio
+import time
+from typing import Callable
+
+
+async def measure_runtime() -> float:
+    """
+    Ejecuta async_comprehension 4 veces en paralelo y devuelve el tiempo total.
+    """
+    async_comprehension = __import__('1-async_comprehension').async_comprehension
+    start = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end = time.perf_counter()
+    return end - start
