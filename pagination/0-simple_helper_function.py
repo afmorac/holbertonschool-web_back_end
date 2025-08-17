583e12a433728f15e6b807d7a0b7f34c9d121055
Andrés M <andresfmora.castro@gmail.com>
2025-08-17 16:35:28 -0400

    1sh

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Función de ayuda para paginación
+"""
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Devuelve el índice inicial y final según la página y su tamaño
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
