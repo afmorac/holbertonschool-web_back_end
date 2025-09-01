24ade09eafcc7097a5b1da1a8815f953551a5e90
Andrés M <andresfmora.castro@gmail.com>
2025-08-31 21:56:21 -0400

    tsk6fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((res) => {
+      if (res.status === 'fulfilled') {
+        return { status: res.status, value: res.value };
+      }
+
+      const msg =
+        res.reason instanceof Error ? res.reason.toString() : String(res.reason);
+      return { status: res.status, value: msg };
+    })
+  );
 }
