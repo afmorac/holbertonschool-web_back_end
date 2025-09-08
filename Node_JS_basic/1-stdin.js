c468c84b766387bc17e5b90e00548a30e742675e
Andrés M <andresfmora.castro@gmail.com>
2025-09-07 23:13:02 -0400

    t1fix/r

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (chunk) => {
+  process.stdout.write('Your name is: ');
+  process.stdout.write(chunk);
 });
 
 process.stdin.on('end', () => {
